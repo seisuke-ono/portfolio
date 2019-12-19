246db3d23fb85edadfca5638f0e9cbe092b130df
seisuke-ono <55654390+seisuke-ono@users.noreply.github.com>
2019-12-19 22:57:09 +0900

    update

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,7 +4,6 @@
     <v-flex xs12 class="content">
       <v-icon x-large>mdi-contacts</v-icon>
       <h2 class="font-weight-bold display-1 mb-3" color="primary">Contact</h2>
-      別途...
     </v-flex>
   </v-layout>
 </v-container>
